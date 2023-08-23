2deefe45a4b43cc7fcc781f2eebd8daed653d94d
Goyo <dur123en@gmail.com>
2023-08-22 19:15:06 -0500

    Initial Commit

---
+++ TabViewExample/TabViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewExampleApp.swift
+//  TabViewExample
+//
+//  Created by Enrique on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TabViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
